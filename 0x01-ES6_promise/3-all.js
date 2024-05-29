2054fa10a043ac658336768e0405d2de46d1aadd
Kwabena Twumasi <ktwum721@gmail.com>
2024-05-29 20:46:27 +0000

    Utils completed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
+    .then(([photo, user]) => {
       console.log(`${photo} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
