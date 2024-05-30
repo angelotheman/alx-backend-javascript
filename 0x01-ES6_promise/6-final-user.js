5d9fe92b70d34ba90ca8853962b6002501030507
Kwabena Twumasi <ktwum721@gmail.com>
2024-05-30 10:52:51 +0000

    Final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
@@ -7,18 +7,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.all([signUpPromise, uploadPromise])
     .then((results) => {
-      const settlementInfo = [
-        { status: results[0].status, value: results[0].value },
-        { status: results[1].status, value: results[1].value },
-      ];
+      const settlementInfo = results.map((result) => ({
+        status: 'fulfilled',
+        value: result,
+      }));
       return settlementInfo;
     })
     .catch((error) => {
       console.error('Error during profile signup:', error);
 
-      return [
+      const errorInfo = [
         { status: 'rejected', value: error },
         { status: 'rejected', value: error },
       ];
+      return errorInfo;
     });
 }
